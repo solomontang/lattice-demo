34a8008a1ff2435e0e9295df32925c66a7de8823
Solomon Tang <solomoncztang@gmail.com>
2018-03-12 15:50:16 -0700

    add currentModel to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import associationTypes from './associationTypes';
 import entityTypes from './entityTypes';
 import propertyTypes from './propertyTypes';
 import isFetching from './isFetching';
+import currentModel from './currentModel';
 
 const rootReducer = combineReducers({
   associationTypes,
   entityTypes,
   propertyTypes,
   isFetching,
+  currentModel,
   router: routerReducer
 });
 
