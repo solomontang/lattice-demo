c1aa032e07f64487669d12efc5b5a5c520a456bb
Solomon Tang <solomoncztang@gmail.com>
2018-03-12 23:57:26 -0700

    listen to LOCATION_CHANGE to set currentModel to null

--- src/reducers/currentModel.js
+++ src/reducers/currentModel.js
@@ -1,9 +1,11 @@
-import { SELECT_MODEL } from '../actions/actionTypes';
+import { SELECT_MODEL, LOCATION_CHANGE } from '../actions/actionTypes';
 
 const currentModel = (state = null, action) => {
   switch(action.type) {
     case SELECT_MODEL:
       return action.id;
+    case LOCATION_CHANGE:
+      return null;
     default:
       return state;
   }
